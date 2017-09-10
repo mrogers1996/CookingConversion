0f5be291c68e2c991572e7510a298364c30868b9
Mandy Rogers <mandyj34@mwc-032121.dhcp.missouri.edu>
2017-09-10 10:57:16 -0500

    blank template

---
+++ CookingConversion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookingConversion
+//
+//  Created by Mandy Rogers on 9/10/17.
+//  Copyright © 2017 Mandy Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
